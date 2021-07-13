bde784e2daeba897084553f1764e699e7ba83ec5
Taiisiia <melnikovataya@gmail.com>
2021-07-13 19:14:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const a=2;
+const b=3;
+console.log(a*b);
+
+const c=15;
+const d=5;
+console.log(c/d);
+
+const e=4;
+const f=6;
+console.log(e+f);
+
+console.log(typeof 11);
+console.log(typeof true);
+console.log(typeof 'java script');
+console.log(typeof '11');
+
+let g=1;
+g+=11;
+console.log (g);
+g-=11;
+console.log (g);
+g*=11;
+console.log (g)
+g/=11;
+console.log (g)
+g+=1;
+console.log (g);
+++g;
+console.log (g);
+--g;
+console.log (g);
+
+
+
+
+ 
